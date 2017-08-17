c8c94a9f151171c57a0a5a8eb34637566e981959
TheCynosure <jack.github@gmail.com>
2017-08-16 21:26:24 -0400

    Split function should work now

--- githubPkgManager/gitpkg.lua
+++ githubPkgManager/gitpkg.lua
@@ -1,10 +1,29 @@
 local repoURLBase = "https://raw.githubusercontent.com/TheCynosure/ComputerCraftScripts/master/"
 local version = 1.0
 
+function split(s, delim)
+	split_s = {}
+	split_index = 1
+	i = 1
+	while true do
+		i = string.find(s, delim)
+		if(i == nil) then
+			break
+		end
+		if(i > 1 and string.sub(s, 1, i - 1) ~= nil) then
+			split_s[split_index] = string.sub(s, 1, i - 1)
+			split_index = split_index + 1
+		end
+		s = string.sub(s, i + 1, string.len(s))
+	end
+	split_s[split_index] = s
+	return split_s
+end
+
 function get(args)
 	res = http.get(repoURLBase .. args[1] .. ".lua")
 	fName = split(args[1], "/")
-	fName = fName[table.getn(fName) - 1]
+	fName = fName[#fName - 1]
 	print("")
 	print("Writing " .. fName)
 	fHandle = fs.open(fName, "w")
