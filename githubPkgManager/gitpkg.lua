bc0613618748fc878853564846394f18666a8f51
TheCynosure <jack.github@gmail.com>
2017-08-16 22:05:41 -0400

    Simplify

--- githubPkgManager/gitpkg.lua
+++ githubPkgManager/gitpkg.lua
@@ -1,3 +1,4 @@
+args={}
 local repoURLBase = "https://raw.githubusercontent.com/TheCynosure/ComputerCraftScripts/master/"
 local version = 1.0
 
@@ -22,36 +23,11 @@ function split(s, delim)
 	return split_s
 end
 
-function get(args)
-	print("Getting " .. repoURLBase .. args[2] .. ".lua")
-	res = http.get(repoURLBase .. args[2] .. ".lua")
-	fName = split(args[2], "/")
-	fName = fName[#fName - 1]
-	print("")
-	print("Writing " .. fName)
-	fHandle = fs.open(fName, "w")
-	fHandle.write(res.readAll())
-	fHandle.close()
-end
-
-function help()
-	print("")
-	print(" -- Command -- ")
-	print("get - Download a file based on its path")
-	print("exit - Exits the program")
-end
-
-
-print("Git Pkg Manager V" .. version .. " by TheCynosure")
-
-while true do
-	--Check for commands
-	write("GPKG> ")
-	local command = read()
-	if(command == "get") then
-		get(split(command, " "))
-	elseif(command == "help") then
-		help()
-	elseif(command == "exit") then break end
-	
-end
+print("Getting " .. repoURLBase .. args[2] .. ".lua")
+res = http.get(repoURLBase .. path .. ".lua")
+fName = split(args[2], "/")
+fName = fName[#fName - 1]
+print("Writing " .. fName)
+fHandle = fs.open(fName, "w")
+fHandle.write(res.readAll())
+fHandle.close()
