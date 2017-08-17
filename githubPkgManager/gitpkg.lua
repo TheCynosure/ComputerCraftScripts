cad91342399457295f553ad3c3c7a7f880456bc3
TheCynosure <jack.github@gmail.com>
2017-08-16 21:48:57 -0400

    What

--- githubPkgManager/gitpkg.lua
+++ githubPkgManager/gitpkg.lua
@@ -5,29 +5,33 @@ function split(s, delim)
 	split_s = {}
 	split_index = 1
 	i = 1
-	while true do
-		i = string.find(s, delim)
-		if(i == nil) then
-			break
+	if(delim ~= nil and s ~= nil) then
+		while true do
+			i = string.find(s, delim, 1, true)
+			if(i == nil) then
+				break
+			end
+			if(i > 1 and string.sub(s, 1, i - 1) ~= nil) then
+				split_s[split_index] = string.sub(s, 1, i - 1)
+				split_index = split_index + 1
+			end
+			s = string.sub(s, i + 1, string.len(s))
 		end
-		if(i > 1 and string.sub(s, 1, i - 1) ~= nil) then
-			split_s[split_index] = string.sub(s, 1, i - 1)
-			split_index = split_index + 1
-		end
-		s = string.sub(s, i + 1, string.len(s))
+		split_s[split_index] = s
 	end
-	split_s[split_index] = s
 	return split_s
 end
 
 function get(args)
-	res = http.get(repoURLBase .. args[1] .. ".lua")
-	fName = split(args[1], "/")
+	print("Getting " .. repoURLBase .. args[2] .. ".lua")
+	res = http.get(repoURLBase .. args[2] .. ".lua")
+	fName = split(args[2], "/")
 	fName = fName[#fName - 1]
 	print("")
 	print("Writing " .. fName)
 	fHandle = fs.open(fName, "w")
 	fHandle.write(res.readAll())
+	fHandle.close()
 end
 
 function help()
@@ -39,13 +43,15 @@ end
 
 
 print("Git Pkg Manager V" .. version .. " by TheCynosure")
+
 while true do
 	--Check for commands
-	write("> ")
+	write("GPKG> ")
 	local command = read()
 	if(command == "get") then
 		get(split(command, " "))
 	elseif(command == "help") then
 		help()
 	elseif(command == "exit") then break end
+	
 end
